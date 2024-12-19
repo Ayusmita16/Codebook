f99f53cdb40037f75a631de2cd4c44f93088216d
Ayusmita <mitra12ayusmita@gmail.com>
2024-12-19 19:58:10 +0530

    deploy fixes

--- src/page/Login.js
+++ src/page/Login.js
@@ -20,6 +20,20 @@ export function Login(props) {
         } catch (error) {
             toast.error(error.message, { closeButton: true, position: "bottom-center" })
         }
+    }
+
+    async function handleGuestLogin() {
+        try {
+            // event.preventDefault();
+            const authDetail = {
+                email: `${process.env.REACT_APP_GUES_LOGIN}`,
+                password: `${process.env.REACT_APP_PASSWORD}`
+            }
+            const data = await login(authDetail);
+            data.accessToken ? nav('/products') : toast.error(data)
+        } catch (error) {
+            toast.error(error.message)
+        }
 
     }
 
@@ -39,7 +53,7 @@ export function Login(props) {
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
             </form>
-            <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
+            <button onClick={handleGuestLogin} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
         </main>
     )
 }
